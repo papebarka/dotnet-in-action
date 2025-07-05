84f14fd78d1ca67ec61378a9057fbe6777787d1c
Pape Barka <papebarka@gmail.com>
2025-07-05 22:03:23 +0000

    Refactors Options.cs

--- asciiart/AsciiArt.cs
+++ asciiart/AsciiArt.cs
@@ -1,10 +1,30 @@
 using Figgle;
+using System.Reflection;
 
 namespace AsciiArt;
 
 public static class AsciiArts{
-    public static void Write(string text){
-        WriteLine(FiggleFonts.Standard.Render(text));
-        WriteLine("Brought to you by " + typeof(AsciiArts).FullName);
+    public static void Write(Options o){
+        FiggleFont? font = null;
+        if (!string.IsNullOrWhiteSpace(o.Font))
+        {
+            font = typeof(FiggleFonts)
+                .GetProperty(o.Font, BindingFlags.Static | BindingFlags.Public)
+                ?.GetValue(null) as FiggleFont;
+
+            if (font == null)
+            {
+                WriteLine($"Could not find font '{o.Font}'");
+            }
+        }
+
+        font ??= FiggleFonts.Standard;
+
+        if (o?.Text != null)
+        {
+            WriteLine(font.Render(o.Text));
+            WriteLine($"Brought to you by {typeof(AsciiArts).FullName}");
+        }
+        
     }
 }
--- asciiart/Program.cs
+++ asciiart/Program.cs
@@ -3,13 +3,8 @@ using CommandLine;
 
 // See https://aka.ms/new-console-template for more 
 
-/*var opts = Parser.Default.ParseArguments<Options>(args);
-
-WriteLine(opts);
-*/
 
 Parser.Default.ParseArguments<Options>(args)
- //.WithParsed(options => WriteLine(options.Text))
- .WithParsed<Options>(options => AsciiArts.Write(options.Text))
+ .WithParsed<Options>(AsciiArts.Write)
  .WithNotParsed(_ =>
    WriteLine("Usage: asciiart <text> --font QUALITY"));
