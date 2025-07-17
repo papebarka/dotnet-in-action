2dffe30c332b69cc0c52c4097d7da54c99314dae
Pape Barka <papebarka@gmail.com>
2025-07-17 14:00:32 +0000

    FindText - Implements FindText logic

--- FindText/Program.cs
+++ FindText/Program.cs
@@ -1,11 +1,55 @@
 ﻿using FindText;
 using CommandLine;
 using CommandLine.Text;
+using System.Security;
 // See https://aka.ms/new-console-template for more information
 
+static void SearchFile (FileInfo file, string text)
+{
+    if (!file.Exists)
+    {
+        Console.Error.WriteLine($"{file.FullName} does not exist!");
+        return;
+    }
+
+    try
+    {
+        using var reader = file.OpenText();
+        string? line;
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            if (line.Contains(text, StringComparison.OrdinalIgnoreCase))
+            {
+                WriteLine(line);
+            }
+        }
+    }
+    catch (UnauthorizedAccessException)
+    {
+        Console.Error.WriteLine($"Unauthorized: {file.FullName}");
+    }
+    catch (IOException)
+    {
+        Console.Error.WriteLine($"IO Error: {file.FullName}");
+    }
+}
+
 var results = Parser.Default.ParseArguments<Options>(args)
-    .WithParsed<Options>(options => {
-        WriteLine(options.Text);
+    .WithParsed<Options>(options =>
+    {
+        if (options.Filename != null)
+        {
+            SearchFile(new FileInfo(options.Filename), options.Text);
+        }
+        else
+        {
+            string? filename;
+            while (!string.IsNullOrWhiteSpace(filename = ReadLine()))
+            {
+                SearchFile(new FileInfo(filename), options.Text!);
+            }
+        }
     });
 
 results.WithNotParsed(_ =>
