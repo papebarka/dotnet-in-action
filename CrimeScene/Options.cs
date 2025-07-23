59b501cfd280ed085d195ffc4ec09285a0917ee1
Pape Barka <papebarka@gmail.com>
2025-07-23 23:53:33 +0000

    CrimeScene - Implementing parsing JSON

--- CrimeScene/Options.cs
+++ CrimeScene/Options.cs
@@ -4,6 +4,13 @@ namespace CrimeScene;
 
 public record Options
 {
-    [Value(0, Required = true, HelpText = "Some Text")]
-    public string? Text { get; init;}
+    [Value(0, Required = true, HelpText = "Input File")]
+    public string? InputFile { get; init; }
+
+    [Value(1, Required = false, HelpText = "Output file")]
+    public string? OutputFile { get; init; }
+
+    [Option('c', "classification", HelpText = "Crime classification")]
+    public string? Classification { get; init; } = "Murder";
+
 }
--- CrimeScene/Program.cs
+++ CrimeScene/Program.cs
@@ -1,11 +1,53 @@
 ﻿using CrimeScene;
 using CommandLine;
 using CommandLine.Text;
+using System.Text;
+using System.Text.Json;
 // See https://aka.ms/new-console-template for more information
 
+static void FindWithJsonDom(Stream inStream, Stream outstream, string classification)
+{
+    using var writer = new StreamWriter(outstream, Encoding.UTF8, leaveOpen: true);
+
+    using var jsonDoc = JsonDocument.Parse(inStream);
+
+    foreach (var jokeElement in jsonDoc.RootElement.EnumerateArray())
+    {
+        string? type = jokeElement.GetProperty("type").GetString();
+
+        if (String.Equals(classification, type, StringComparison.OrdinalIgnoreCase))
+        {
+            writer.WriteLine(jokeElement.GetRawText());
+        }
+    }
+}
+
 var results = Parser.Default.ParseArguments<Options>(args)
-    .WithParsed<Options>(options => {
-        WriteLine(options.Text);
+    .WithParsed<Options>(options =>
+    {
+        var inFile = new FileInfo(options.InputFile!);
+        if (!inFile.Exists)
+        {
+            Console.Error.WriteLine($"{inFile.FullName} does not exist.");
+            return;
+        }
+
+        using var inStream = inFile.OpenRead();
+
+        FileInfo? outFile = null;
+        if (options.OutputFile != null)
+        {
+            outFile = new FileInfo(options.OutputFile);
+
+            if (outFile.Exists)
+            {
+                outFile.Delete();
+            }
+        }
+
+        using var outStream = outFile != null ? outFile.OpenWrite() : Console.OpenStandardOutput();
+
+        FindWithJsonDom(inStream, outStream, options.Classification);
     });
 
 results.WithNotParsed(_ =>
